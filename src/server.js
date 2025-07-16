1439e232545f765017c13bb133b7e0919b845481
Soto Hatakeyama <hataso-b1030@keio.jp>
2025-07-16 09:55:06 +0900

    Force update to sync files

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 // Prisma Clientをインポート
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 // Prisma Clientのインスタンスを作成
 const prisma = new PrismaClient();
@@ -9,20 +9,21 @@ const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // '/api/terms'のエンドポイントをasync/awaitを使って書き換える
-app.get('/api/terms', async (req, res) => {
-    try {
-        // Prismaを使ってデータベースから全ての用語を取得
-        const terms = await prisma.stockTerm.findMany();
-        res.json(terms);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'データベースからの取得に失敗しました。' });
-    }
+app.get("/api/terms", async (req, res) => {
+  try {
+    // Prismaを使ってデータベースから全ての用語を取得
+    const terms = await prisma.stockTerm.findMany();
+    res.json(terms);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "データベースからの取得に失敗しました。" });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`サーバーがポート${PORT}で起動しました。`);
+  console.log(`サーバーがポート${PORT}で起動しました。`);
 });
+// test commit
